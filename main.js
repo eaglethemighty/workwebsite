318fd8d831a8998f5587419f47a5a357f24aa83e
moszroute <67115774+amichalakpoppa@users.noreply.github.com>
2021-10-28 18:51:21 +0200

    Import welcome animation multimedia and fill in html"

--- main.js
+++ main.js
@@ -1,7 +1,11 @@
 const menuButton = document.querySelector('.nav_menu_button');
 const menuItems = document.querySelector('.nav_menu_list');
+const animationPicture = document.querySelector('.introduction_picture');
+const animationStartButton = document.querySelector('.animation_compilation');
+const globalSettings = document.querySelector('html');
 
 let menuOpen = true;
+let welcomeAnimationStarted = false;
 
 menuButton.addEventListener('click', () => {
     if(!menuOpen) {
@@ -23,3 +27,30 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
         });
     });
 });
+
+animationStartButton.addEventListener('click', function (e) {
+    e.preventDefault();
+
+    if(!welcomeAnimationStarted)
+    {
+        welcomeAnimationStarted = true;
+
+        globalSettings.classList?.add('waiting_cursor')
+        WaitForCompilationAnimation();
+    }
+
+
+});
+
+function WaitForCompilationAnimation() {
+    setTimeout(function () {
+        if (animationPicture.src == "multimedia/visual_studio_build.PNG") {
+            return;
+        }
+
+        else {
+            animationPicture.src = "multimedia/visual_studio_build.PNG";
+            globalSettings.classList?.remove('waiting_cursor')
+        }
+    }, 2000);
+}
